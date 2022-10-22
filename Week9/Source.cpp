830e3db28f1cbe559ea1d127b667fc4fcf8ba2ef
Aceramey <aceramey02@gmail.com>
2022-10-21 23:28:25 -0400

    First commit

--- Week9/Source.cpp
+++ Week9/Source.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	string letters[12] = { "H", "e", "l", "l", "o", " ", "w", "o", "r","l","d","!" };
+	string letters[12] = { "H", "e", "l", "l", "o", " ", "w", "o", "r","l","d","!"};
 	for (int i = 0; i < 12; i++)
 	{
 		cout << letters[i];
