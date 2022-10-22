206d71bd59a89307b2011ba3ba5a69c53d4675a9
acera <acera@COOPER>
2022-10-21 23:19:03 -0400

    Add project files.

---
+++ Week9/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string letters[12] = { "H", "e", "l", "l", "o", " ", "w", "o", "r","l","d","!" };
+	for (int i = 0; i < 12; i++)
+	{
+		cout << letters[i];
+	}
+}
