dc62ee64bbb6bc4e78d97e84b62d5d57683e5835
Aceramey <60988178+Aceramey@users.noreply.github.com>
2022-10-21 23:30:26 -0400

    Changed target audience

--- Week9/Source.cpp
+++ Week9/Source.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	string letters[12] = { "H", "e", "l", "l", "o", " ", "w", "o", "r","l","d","!"};
+	string letters[12] = { "H", "e", "l", "l", "o", " ", "u", "n", "i","v","e","r","s","e","!"};
 	for (int i = 0; i < 12; i++)
 	{
 		cout << letters[i];
